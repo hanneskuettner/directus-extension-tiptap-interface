c84995bcb0a2ada50c33457a7fe467daf72623ef
Hannes Küttner <kuettner.hannes@gmail.com>
2023-03-30 19:25:23 +0200

    🔧 Change interface description

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import TipTapEditor from './components/tiptap-editor.vue';
 export default defineInterface({
 	id: 'tiptap-editor',
 	name: 'Block Editor',
-	icon: 'box',
-	description: '',
+	icon: 'dns',
+	description: 'A Notion-like editor experience',
 	component: TipTapEditor,
 	options: null,
 	types: ['json'],
